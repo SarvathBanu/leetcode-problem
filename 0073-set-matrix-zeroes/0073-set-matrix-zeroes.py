8b8f79d169f291d55993ecc1b503a454cb7bfb29
SarvathBanu <sarvathbanushajath@gmail.com>
2026-07-22 22:15:07 +0530

    Time: 226 ms (14.31%), Space: 22.5 MB (7.75%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def setZeroes(self, matrix):
+        zero = []
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    zero.append((i, j))
+
+        for i, j in zero:
+            for k in range(len(matrix[0])):
+                matrix[i][k] = 0
+            for k in range(len(matrix)):
+                matrix[k][j] = 0
